f1d188e2c93619f14f0e0579a4a525e3017affce
Sergey Ukhanov <sergey.ukhanov@gmail.com>
2025-08-14 21:35:37 -0400

    Task 1:  Modify SampleClient so that it prints the first and last name, and birth date of each Patient to the screen  Sort the output so that the results are ordered by the patient's first name

--- src/main/java/SampleClient.java
+++ src/main/java/SampleClient.java
@@ -2,7 +2,11 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import org.hl7.fhir.r4.model.Bundle;
+import org.hl7.fhir.r4.model.HumanName;
 import org.hl7.fhir.r4.model.Patient;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class SampleClient {
 
@@ -21,7 +25,55 @@ public class SampleClient {
                 .returnBundle(Bundle.class)
                 .execute();
 
-
+        // Sort the response by patience's first name since the order is not guarantied
+        sortBundleEntriesByFirstName(response);
+        // Process the response for printing names and DOBs
+        printPatientsNameAndDOB(response);
     }
 
+    public static void sortBundleEntriesByFirstName(Bundle bundle) {
+        if (bundle == null || bundle.getEntry() == null || bundle.getEntry().isEmpty()) {
+            return;
+        }
+
+        List<Bundle.BundleEntryComponent> entries = bundle.getEntry();
+
+        // Sort the entries using a custom Comparator
+        entries.sort(new Comparator<Bundle.BundleEntryComponent>() {
+            @Override
+            public int compare(Bundle.BundleEntryComponent entryFirst, Bundle.BundleEntryComponent entrySecond) {
+
+                Patient patientFirst = (Patient) entryFirst.getResource();
+                Patient patientSecond = (Patient) entrySecond.getResource();
+
+                if (patientFirst == null && patientSecond == null) {
+                    return 0;
+                } else if (patientFirst == null) {
+                    return 1;
+                } else if (patientSecond == null) {
+                    return -1;
+                }
+
+                String nameFirst = patientFirst.getNameFirstRep().getGivenAsSingleString();
+                String nameSecond = patientSecond.getNameFirstRep().getGivenAsSingleString();
+
+                return nameFirst.compareTo(nameSecond);
+            }
+        });
+    }
+
+    public static void printPatientsNameAndDOB(Bundle bundle) {
+        bundle.getEntry().stream()
+                .map(entry -> (Patient) entry.getResource())
+                .forEach(patient -> {
+                    // the name may consist of multiple names
+                    String name = patient.getName().stream()
+                            .map(HumanName::getNameAsSingleString)
+                            .findFirst()
+                            .orElse("NO NAME");
+
+                    String dateOfBirth = patient.hasBirthDate() ? patient.getBirthDate().toString() : "UKNOWN";
+                    System.out.println(name + ", " + dateOfBirth);
+                });
+    }
 }
